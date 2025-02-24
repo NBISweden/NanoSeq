1e42d2ce1c7d9d538bed166c1f8cf137ae334fcb
CormacKinsella <27350062+CormacKinsella@users.noreply.github.com>
2025-02-24 13:26:26 +0100

    remove opt builds in favour of hosted docker image

--- build/opt-build.sh
+++
@@ -1,9 +0,0 @@
-
-
-# Remaining to containerise:
-
-- bcftools
-- potentially libdeflate
-- verifyBamID (https://github.com/Griffan/VerifyBamID/archive/$VER_VERIFYBAMID.tar.gz)
-- R libraries/dependencies
-- gz stream
