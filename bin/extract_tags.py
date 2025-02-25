dce0abd554e9c6283b9ac4266646a74bc9264315
CormacKinsella <27350062+CormacKinsella@users.noreply.github.com>
2025-02-25 11:58:55 +0100

    test refactor extracttags.py, attempting to prevent occasional error 141

--- bin/extract_tags.py
+++ bin/extract_tags.py
@@ -72,132 +72,115 @@ from the read sequence to rb and mb auxiliary tags.
 
 
 class Read:
+    """Class representing one read from a paired-end read."""
 
-    """Class representing one read from a paired-end read.
-    """
-
-    def __init__(self, readlength):
-        """Initialises with an empty header, sequence, quality and tag.
-        """
-        self.readlength = readlength
+    def __init__(self, read_length):
+        """Initializes with an empty header, sequence, quality, and tag."""
+        self.read_length = read_length
         self.header = None
         self.sequence = None
         self.quality = None
         self.tag = None
 
     def add_header(self, header):
-        """Add header string minus i5/i7 barcode.
-        """
+        """Add header string minus i5/i7 barcode."""
         self.header = header.strip().split(' ')[0]
 
     def add_sequence(self, sequence):
-        """Add sequence string.
-        """
+        """Add sequence string."""
         self.sequence = sequence.strip()
 
     def add_quality(self, quality):
-        """Add quality string.
-        """
+        """Add quality string."""
         self.quality = quality.strip()
 
     def add_tag(self, matches):
-        """Add first [matches] bases to tag.
-        """
+        """Add first [matches] bases to tag."""
         self.tag = self.sequence[:matches]
         assert len(self.tag) == matches
 
     def trim_sequence(self, skips):
-        """Trim adaptor from sequence string.
-        """
+        """Trim adaptor from sequence string."""
         self.sequence = self.sequence[skips:]
-        assert len(self.sequence) == self.readlength - skips
+        assert len(self.sequence) == self.read_length - skips
 
     def trim_quality(self, skips):
-        """Trim adaptor from quality string.
-        """
+        """Trim adaptor from quality string."""
         self.quality = self.quality[skips:]
-        assert len(self.quality) == self.readlength - skips
+        assert len(self.quality) == self.read_length - skips
 
     def amend_header(self, tag):
-        """Add rb and mb auxiliary tags to the header.
-        """
-        self.header = '%s\trb:Z:%s\tmb:Z:%s' % (self.header, self.tag, tag)
+        """Add rb and mb auxiliary tags to the header."""
+        self.header = f'{self.header}\trb:Z:{self.tag}\tmb:Z:{tag}'
 
 
 class ExtractTags:
-
-    def __init__(self, in1, in2, out1, out2, matches, skips, readlength):
-        """Initiate extract tags object.
-        """
-        if ( in1.endswith('.gz') and (in2.endswith('.gz')) ) :
-          self.in1 = gzip.open(in1, 'rt')
-          self.in2 = gzip.open(in2, 'rt')
-          self.out1 = gzip.open(out1, 'wt', compresslevel=2)
-          self.out2 = gzip.open(out2, 'wt', compresslevel=2)
-        else :
-          self.in1 = open(in1, 'r')
-          self.in2 = open(in2, 'r')
-          self.out1 = open(out1, 'w')
-          self.out2 = open(out2, 'w')
-
+    def __init__(self, in1, in2, out1, out2, matches, skips, read_length):
+        """Initiate extract tags object."""
         self.matches = matches
         self.tags = self.canonical_tags()
         self.skips = matches + skips
-        self.readlength = readlength
+        self.read_length = read_length
+
+        self.open_files(in1, in2, out1, out2)
         self.extract_tags()
 
+    def open_files(self, in1, in2, out1, out2):
+        """Open input and output files."""
+        mode_in = 'rt' if in1.endswith('.gz') else 'r'
+        mode_out = 'wt' if out1.endswith('.gz') else 'w'
+        self.in1 = gzip.open(in1, mode_in) if in1.endswith('.gz') else open(in1, mode_in)
+        self.in2 = gzip.open(in2, mode_in) if in2.endswith('.gz') else open(in2, mode_in)
+        self.out1 = gzip.open(out1, mode_out, compresslevel=2) if out1.endswith('.gz') else open(out1, mode_out)
+        self.out2 = gzip.open(out2, mode_out, compresslevel=2) if out2.endswith('.gz') else open(out2, mode_out)
+
     def canonical_tags(self):
-        """Returns a list of tags of size matches that consists of canonical A, C,
-        G, T bases.
-        """
-        tags = [''.join(i)
-                for i in itertools.product('ACGT', repeat=self.matches)]
-        assert len(tags) == 4**self.matches
+        """Returns a list of tags of size matches that consists of canonical A, C, G, T bases."""
+        tags = [''.join(i) for i in itertools.product('ACGT', repeat=self.matches)]
+        assert len(tags) == 4 ** self.matches
         return tags
 
     def extract_tags(self):
-        """Extract tags and re-write fastq files.
-        """
-        count = 0
-        for header1 in self.in1:
-            r1 = Read(self.readlength)
-            r2 = Read(self.readlength)
-            # header
-            r1.add_header(header1)
-            r2.add_header(self.in2.readline())
-            if r1.header != r2.header:
-                print(f"Mismatched headers:\nR1: {r1.header}\nR2: {r2.header}")
-                raise ValueError('File is not in sync')
-            # sequence
-            r1.add_sequence(self.in1.readline())
-            r2.add_sequence(self.in2.readline())
-            # spacer
-            self.in1.readline()
-            self.in2.readline()
-            # quality
-            r1.add_quality(self.in1.readline())
-            r2.add_quality(self.in2.readline())
-            # amend header
-            r1.add_tag(self.matches)
-            r2.add_tag(self.matches)
-            # ignore tags with non-canonical bases
-            if r1.tag in self.tags and r2.tag in self.tags:
-                r1.trim_sequence(self.skips)
-                r2.trim_sequence(self.skips)
-                r1.trim_quality(self.skips)
-                r2.trim_quality(self.skips)
-                r1.amend_header(r2.tag)
-                r2.amend_header(r1.tag)
-                self.out1.write('%s\n%s\n+\n%s\n' % (r1.header, r1.sequence,
-                                                     r1.quality))
-                self.out2.write('%s\n%s\n+\n%s\n' % (r2.header, r2.sequence,
-                                                     r2.quality))
-        if (self.in2.readline() != "" ) :
-          raise ValueError('Files are not in sync')
+        """Extract tags and re-write fastq files."""
+        try:
+            for header1 in self.in1:
+                r1 = Read(self.read_length)
+                r2 = Read(self.read_length)
+                r1.add_header(header1)
+                r2_header = self.in2.readline()
+                r2.add_header(r2_header)
+                if r1.header != r2.header:
+                    raise ValueError(f"Mismatched headers:\nR1: {r1.header}\nR2: {r2_header.strip()}")
+
+                r1.add_sequence(self.in1.readline())
+                r2.add_sequence(self.in2.readline())
+                _ = self.in1.readline(), self.in2.readline()  # ignore spacer lines
+                r1.add_quality(self.in1.readline())
+                r2.add_quality(self.in2.readline())
+                r1.add_tag(self.matches)
+                r2.add_tag(self.matches)
+
+                if r1.tag in self.tags and r2.tag in self.tags:
+                    r1.trim_sequence(self.skips)
+                    r2.trim_sequence(self.skips)
+                    r1.trim_quality(self.skips)
+                    r2.trim_quality(self.skips)
+                    r1.amend_header(r2.tag)
+                    r2.amend_header(r1.tag)
+                    self.out1.write(f"{r1.header}\n{r1.sequence}\n+\n{r1.quality}\n")
+                    self.out2.write(f"{r2.header}\n{r2.sequence}\n+\n{r2.quality}\n")
+            if self.in2.readline():
+                raise ValueError('R2 file has additional reads not present in R1')
+        finally:
+            self.in1.close()
+            self.in2.close()
+            self.out1.close()
+            self.out2.close()
+
 
 if __name__ == "__main__":
-    msg = ("Extract tags from fasta files.  For sonicated library use -m 3 -s "
-           "2.  For HpyCH4V/AluI library use -m 3 -s 4.")
+    msg = ("Extract tags from fasta files. For sonicated library use -m 3 -s "
+           "2. For HpyCH4V/AluI library use -m 3 -s 4.")
     parser = argparse.ArgumentParser(description=msg)
     parser.add_argument('-a', required=True, help="Input fastq file R1")
     parser.add_argument('-b', required=True, help="Input fastq file R2")
